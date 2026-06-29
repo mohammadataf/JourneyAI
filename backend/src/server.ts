42770871aa33e0c21c8d241134fccee0f8a1557e
mohammadataf <atafmohd32@gmail.com>
2026-06-30 03:53:31 +0530

    feat(auth): setup backend foundation and authentication routing

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`
+====================================
+🚀 JourneyAI Backend Started
+🌍 Environment : Development
+📡 Server      : http://localhost:${PORT}
+====================================
+`);
+});
