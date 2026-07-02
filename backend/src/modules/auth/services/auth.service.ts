2ea928cbeee47697c5c3cbc2cd3085b8ab457cef
mohammadataf <atafmohd32@gmail.com>
2026-07-03 02:41:12 +0530

    feat(auth): add duplicate email validation

--- backend/src/modules/auth/services/auth.service.ts
+++ backend/src/modules/auth/services/auth.service.ts
@@ -4,15 +4,38 @@ type RegisterUserData = {
     password: string;
 };
 
+const users = [
+    {
+        id: 1,
+        name: "Admin User",
+        email: "admin@journeyai.com",
+    },
+    {
+        id: 2,
+        name: "Mohammad Ataf",
+        email: "ataf@gmail.com",
+    },
+];
+
 export const registerUserService = (user: RegisterUserData) => {
+
+    const existingUser = users.find(
+        (currentUser) => currentUser.email === user.email
+    );
+
+    if (existingUser) {
+        return {
+            success: false,
+            message: "Email already exists.",
+        };
+    }
 
     return {
         success: true,
         message: "Registration request processed successfully.",
         data: {
             name: user.name,
-            email: user.email
-        }
+            email: user.email,
+        },
     };
-
 };
